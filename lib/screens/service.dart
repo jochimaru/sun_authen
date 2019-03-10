480581d452d74d18fb92700df24762dbed0a69c3
Kumpol <kumpol.p@gmail.com>
2019-03-10 15:57:50 +0700

    Pass data between screen

---
+++ lib/screens/service.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Service extends StatefulWidget {
+  final String nameLoginString;
+
+  Service({Key key, this.nameLoginString}) : super(key:key);
+
+  @override
+  _ServiceState createState() => _ServiceState();
+}
+
+class _ServiceState extends State<Service> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${widget.nameLoginString}'),
+      ),
+      body: Text('Welcome ${widget.nameLoginString}'),
+    );
+  }
+}
